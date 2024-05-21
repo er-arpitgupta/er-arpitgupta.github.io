99180996af552d77f45a02ab11a7358b8654ab57
Arpit Gupta <arpit.gupta1@gds.ey.com>
2024-05-21 22:50:44 +0530

    changes made

--- index.js
+++ index.js
@@ -1,30 +1,114 @@
-var isDarkMode = false;
-
-$('.darkmode-parent label').click(function () {
-    isDarkMode = isDarkMode ? false : true;
+$('.darkmode label').click(function () {
     $(document.body).toggleClass('dark');
-    if (isDarkMode) {
-        $('.sun').css('display', 'none');
-        $('.moon').css('display', 'block');
+})
 
-        $('.lightmode-animation').css('display', 'none');
-        $('.darkmode-animation').css('display', 'block');
-    } else {
-        $('.sun').css('display', 'block');
-        $('.moon').css('display', 'none');
+function updateClock() {
+    const now = new Date();
 
-        $('.lightmode-animation').css('display', 'block');
-        $('.darkmode-animation').css('display', 'none');
+    let hours = now.getHours();
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    const strTime = `${String(hours).padStart(2, '0')}:${minutes} ${ampm}`;
+
+    const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+    const strDate = now.toLocaleDateString('en-GB', options);
+
+    document.getElementById('time').textContent = strTime;
+    document.getElementById('date').textContent = strDate;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
+$(".navbar ul li").click(function (e) {
+    $('.navbar ul li span').css('display', 'none');
+    $(`.navbar ul li.${e.target.classList.value.split(' ')[0]} span`).css('display', 'block');
+    $("html, body").animate(
+        {
+            scrollTop: $(`.container #${e.target.classList.value.split(' ')[0]}`).offset().top,
+        },
+        "slow"
+    );
+});
+
+$(".links img").click(function (e) {
+    window.open(
+        e.target.alt === "gmail"
+            ? "mailto:er.gupta.arpit@gmail.com"
+            : e.target.alt === "linkedin"
+                ? "https://www.linkedin.com/in/er-arpit-gupta"
+                : "https://github.com/er-arpitgupta"
+    );
+});
+
+function calculateMonths(startDate, endDate) {
+    const map = {
+        Jan: 1,
+        Feb: 2,
+        Mar: 3,
+        Apr: 4,
+        May: 5,
+        Jun: 6,
+        Jul: 7,
+        Aug: 8,
+        Sep: 9,
+        Oct: 10,
+        Nov: 11,
+        Dec: 12,
+    };
+    const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+
+    const startDateParts = startDate.split("/");
+    const endDateParts =
+        endDate === "Present"
+            ? [months[new Date().getMonth()], new Date().getFullYear()]
+            : endDate.split("/");
+    const monCount =
+        (parseInt(endDateParts[1], 10) - parseInt(startDateParts[1], 10)) * 12 +
+        (map[endDateParts[0]] - map[startDateParts[0]]) +
+        1;
+
+    if (monCount > 12) {
+        return Math.floor(monCount / 12) + " yrs " + (monCount % 12) + " mon";
     }
-})
+    return monCount + " mon";
+}
 
-$('.banner .menu li').click(function (e) {
-    if (e.target.localName == 'li') $(e.target.id)[0].scrollIntoView();
-    else if (e.target.localName == 'img') $(`#${e.target.alt}`)[0].scrollIntoView();
-    else $(`#${e.target.innerHTML.toLowerCase()}`)[0].scrollIntoView();
-})
+$(".calc1").text(calculateMonths($(".start1").text(), $(".end1").text()));
+$(".calc2").text(calculateMonths($(".start2").text(), $(".end2").text()));
+$(".calc3").text(calculateMonths($(".start3").text(), $(".end3").text()));
 
-$('.contact .links img').click(function (e) {
+
+$(window).scroll(function () {
+    var allNavLi = [];
+    $(".navbar li").each(function () {
+        allNavLi.push($(this).attr("class").split(' ')[0]);
+    });
+
+    for (let i = 0; i < allNavLi.length; i++) {
+        if ($(window).scrollTop() >= $(`#${allNavLi[i]}`).offset().top) {
+            $('.navbar ul li span').css('display', 'none');
+            $(`.navbar ul li.${allNavLi[i]} span`).css('display', 'block');
+        }
+    }
+});
+
+$('.about-section .links img').click(function (e) {
     window.open(
         e.target.alt === 'gmail'
             ? 'mailto:er.gupta.arpit@gmail.com'
@@ -33,3 +117,6 @@ $('.contact .links img').click(function (e) {
                 : 'https://github.com/er-arpitgupta'
     )
 })
+
+
+$('footer .curr_yr').text(' ' + new Date().getFullYear());
