be7187a754218ea9d7a4ff59723739584de8324f
Arpit Gupta <Arpit.Gupta1@gds.ey.com>
2024-07-19 12:58:52 +0530

    adding changes

--- index.js
+++ index.js
@@ -1,109 +1,98 @@
 function updateClock() {
-  const t = new Date();
-  let e = t.getHours();
-  const a = String(t.getMinutes()).padStart(2, "0"),
-    n = e >= 12 ? "PM" : "AM";
-  (e %= 12), (e = e || 12);
-  const l = `${String(e).padStart(2, "0")}:${a} ${n}`,
-    o = t.toLocaleDateString("en-GB", {
-      weekday: "short",
-      year: "numeric",
-      month: "short",
-      day: "numeric",
+  const now = new Date();
+
+  let hours = now.getHours();
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  const strTime = `${String(hours).padStart(2, "0")}:${minutes} ${ampm}`;
+
+  const options = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+  const strDate = now.toLocaleDateString("en-GB", options);
+  $(".time").text(strTime);
+  $(".date").text(strDate);
+}
+
+function calculateMonths(element) {
+  const start = $(element).data("start");
+  const end = $(element).data("end");
+  var startDate = new Date(start);
+  var endDate = end === "present" ? new Date() : new Date(end);
+
+  var monthsDifference =
+    (endDate.getFullYear() - startDate.getFullYear()) * 12 +
+    (endDate.getMonth() - startDate.getMonth());
+
+  const yrDiff = parseInt(monthsDifference / 12);
+  const monDiff = (monthsDifference % 12) + 1;
+
+  $(element).text(
+    $(element).text() +
+      (yrDiff > 0 ? yrDiff + " yrs" : "") +
+      " " +
+      (monDiff && monDiff + " months")
+  );
+}
+
+$(document).ready(function () {
+  $(".darkmode label").click(function () {
+    $(document.body).toggleClass("dark");
+  });
+
+  setInterval(updateClock, 1000);
+  updateClock();
+
+  $(".duration").each(function (index, element) {
+    calculateMonths(element);
+  });
+
+  $("nav ul li").click(function (e) {
+    $("nav ul li span").css("display", "none");
+    $(`nav ul li.${e.target.classList.value.split(" ")[0]} span`).css(
+      "display",
+      "block"
+    );
+    $("html, body").animate(
+      {
+        scrollTop:
+          $(`.container #${e.target.classList.value.split(" ")[0]}`).offset()
+            .top - 150,
+      },
+      "slow"
+    );
+  });
+
+  $(window).scroll(function () {
+    var allNavLi = [];
+    $("nav li").each(function () {
+      allNavLi.push($(this).attr("class").split(" ")[0]);
     });
-  $(".time").text(l), $(".date").text(o);
-}
-function calculateMonths(t, e) {
-  const a = {
-      Jan: 1,
-      Feb: 2,
-      Mar: 3,
-      Apr: 4,
-      May: 5,
-      Jun: 6,
-      Jul: 7,
-      Aug: 8,
-      Sep: 9,
-      Oct: 10,
-      Nov: 11,
-      Dec: 12,
-    },
-    n = t.split("/"),
-    l =
-      "Present" === e
-        ? [
-            [
-              "Jan",
-              "Feb",
-              "Mar",
-              "Apr",
-              "May",
-              "Jun",
-              "Jul",
-              "Aug",
-              "Sep",
-              "Oct",
-              "Nov",
-              "Dec",
-            ][new Date().getMonth()],
-            new Date().getFullYear(),
-          ]
-        : e.split("/"),
-    o =
-      12 * (parseInt(l[1], 10) - parseInt(n[1], 10)) + (a[l[0]] - a[n[0]]) + 1;
-  return o > 12 ? Math.floor(o / 12) + " yrs " + (o % 12) + " mon" : o + " mon";
-}
-$(".darkmode label").click(function () {
-  $(document.body).toggleClass("dark");
-}),
-  setInterval(updateClock, 1e3),
-  updateClock(),
-  $(".navbar ul li").click(function (t) {
-    $(".navbar ul li span").css("display", "none"),
-      $(`.navbar ul li.${t.target.classList.value.split(" ")[0]} span`).css(
-        "display",
-        "block"
-      ),
-      $("html, body").animate(
-        {
-          scrollTop:
-            $(`.container #${t.target.classList.value.split(" ")[0]}`).offset()
-              .top - 150,
-        },
-        "slow"
-      );
-  }),
-  $(".links img").click(function (t) {
+
+    for (let i = 0; i < allNavLi.length; i++) {
+      if ($(window).scrollTop() >= $(`#${allNavLi[i]}`).offset().top - 500) {
+        $("nav ul li span").css("display", "none");
+        $(`nav ul li.${allNavLi[i]} span`).css("display", "block");
+      }
+    }
+  });
+
+  $(".links img").click(function (e) {
     window.open(
-      "gmail" === t.target.alt
+      e.target.alt === "gmail"
         ? "mailto:er.gupta.arpit@gmail.com"
-        : "linkedin" === t.target.alt
+        : e.target.alt === "linkedin"
         ? "https://www.linkedin.com/in/er-arpit-gupta"
-        : "github" === t.target.alt
+        : e.target.alt === "github"
         ? "https://github.com/er-arpitgupta"
         : "https://codepen.io/er-arpitgupta"
     );
-  }),
-  $(".calc1").text(calculateMonths($(".start1").text(), $(".end1").text())),
-  $(".calc2").text(calculateMonths($(".start2").text(), $(".end2").text())),
-  $(".calc3").text(calculateMonths($(".start3").text(), $(".end3").text())),
-  $(window).scroll(function () {
-    var t = [];
-    $(".navbar li").each(function () {
-      t.push($(this).attr("class").split(" ")[0]);
-    });
-    for (let e = 0; e < t.length; e++)
-      $(window).scrollTop() >= $(`#${t[e]}`).offset().top - 500 &&
-        ($(".navbar ul li span").css("display", "none"),
-        $(`.navbar ul li.${t[e]} span`).css("display", "block"));
-  }),
-  $(".about-section .links img").click(function (t) {
-    window.open(
-      "gmail" === t.target.alt
-        ? "mailto:er.gupta.arpit@gmail.com"
-        : "linkedin" === t.target.alt
-        ? "https://www.linkedin.com/in/er-arpit-gupta"
-        : "https://github.com/er-arpitgupta"
-    );
-  }),
+  });
+
   $("footer .curr_yr").text(" " + new Date().getFullYear());
+});
